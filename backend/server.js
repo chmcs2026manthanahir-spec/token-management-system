2606797944a970cfef24740521c7eadf8ab11610
chmcs2026manthanahir-spec <chmcs2026.manthan.ahir@gmail.com>
2026-03-10 23:50:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  //origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type"],
 }));
