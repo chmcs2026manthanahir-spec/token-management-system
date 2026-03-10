d10cd440b514e4d41175f202f032efa7637c766b
chmcs2026manthanahir-spec <chmcs2026.manthan.ahir@gmail.com>
2026-03-10 23:47:27 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://token-management-system-backend.onrender.com",
 });
 
 export default axiosInstance;
